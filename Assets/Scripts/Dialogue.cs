e7b8e8859f87b4017082e5586c6c3dff5ef2e6f4
yunusertgr <104680981+YunusErtugrul@users.noreply.github.com>
2023-06-22 23:14:56 +0300

    0.04v
    ai2,object info,radio,name change...

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text textOB;
+    public GameObject Activator;
+    public string dialogue = "Dialogue";
+
+    public float timer = 2f;
+    public GameObject blackbackground;
+
+
+
+    void Start()
+    {
+        textOB.GetComponent<Text>().enabled = false;
+        blackbackground.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textOB.GetComponent<Text>().enabled = true;
+            textOB.text = dialogue.ToString();
+            blackbackground.SetActive(true);
+            StartCoroutine(DisableText());
+        }
+    }
+
+    IEnumerator DisableText()
+    {
+        yield return new WaitForSeconds(timer);
+        textOB.GetComponent<Text>().enabled = false;
+        blackbackground.SetActive(false);
+        Destroy(Activator);
+
+    }
+
+
+}
---
+++ Assets/Scripts/objectInfo.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectInfo : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject noteUI;
+    //public GameObject hud;
+    public GameObject inv;
+
+    public GameObject pickUpText;
+    public GameObject exitText;
+
+    public AudioSource pickUpSound;
+
+    public bool inReach;
+
+    public bool alReady;
+    public AudioSource takeSound;
+    public Animator takeAI;
+
+    void Start()
+    {
+        noteUI.SetActive(false);
+        //hud.SetActive(true);
+        inv.SetActive(true);
+        pickUpText.SetActive(false);
+        exitText.SetActive(false);
+        inReach = false;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+        }
+    }
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inReach && alReady == false)
+        {
+            takeSound.Play();
+            StartCoroutine(pressE());
+            takeAI.SetTrigger("take");
+            takeAI.ResetTrigger("exit");
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && alReady)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            noteUI.SetActive(false);
+            //hud.SetActive(true);
+            inv.SetActive(true);
+            //player.GetComponent<FirstPersonController>().enabled = true;
+            alReady = false;
+            pickUpText.SetActive(true);
+            exitText.SetActive(false);
+            Time.timeScale = 1;
+            takeAI.SetTrigger("exit");
+            takeAI.ResetTrigger("take");
+        }
+    }
+
+    IEnumerator pressE()
+    {
+        yield return new WaitForSeconds(1);
+        noteUI.SetActive(true);
+        pickUpSound.Play();
+        pickUpText.SetActive(false);
+        inv.SetActive(false);
+        //player.GetComponent<FirstPersonController>().enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        alReady = true;
+        exitText.SetActive(true);
+        //Time.timeScale = 0;
+    }
+
+
+    public void ExitButton()
+    {
+
+
+    }
+}
